7e7437ef77b67e51e52f5adc1bf9aec944272c18
Jack DiSalvatrore <jackp.disalvatore@gmail.com>
2018-04-18 01:39:54 +0000

    Adding user profiles

--- models/user.js
+++ models/user.js
@@ -3,6 +3,9 @@ var passportLocalMongoose = require("passport-local-mongoose");
 
 var UserSchema = new mongoose.Schema({
     username: String,
+    email: String,
+    avatar: String,
+    bio: {type: String, default: "No bio yet..."},
     password: String,
     isAdmin: { type: Boolean, default : false }
 });
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,8 @@
-var express = require("express");
-var router = express.Router();
-var User = require("../models/user");
-var passport = require("passport");
+var express    = require("express");
+var router     = express.Router();
+var User       = require("../models/user");
+var Campground = require("../models/campground");
+var passport   = require("passport");
 
 
 router.get("/", function(req, res){
@@ -19,7 +20,12 @@ router.get("/register", function(req, res){
 
 /* Handles signup logic */
 router.post("/register", function(req, res){
-    var newUser = new User({username: req.body.username});
+    var newUser = new User({
+        username: req.body.username,
+        email: req.body.email,
+        avatar: req.body.avatar,
+        bio: req.body.bio
+    });
     
     /* Should probably use an env variable for this? */
     if(req.body.admincode == "secretcode1234") {
@@ -50,7 +56,7 @@ router.post("/login", passport.authenticate("local", {
         successRedirect: "/campgrounds",
         failureRedirect: "/login"
     }), function(req, res){
-    
+
 });
 
 /* LOGOUT */
@@ -59,5 +65,55 @@ router.get("/logout", function(req, res){
     req.flash("success", "Logged you out!");
     res.redirect("/campgrounds");
 });
+
+/*======================================================
+                        USER PROFILE
+=======================================================*/
+
+/* SHOW - User profile */
+router.get("/user/:id", function(req, res) {
+    User.findById(req.params.id, function(err, foundUser){
+        if(err) {
+            req.flash("error", "Error finding user");
+            res.redirect("/");
+        } else {
+            /* Find the campgrounds that the user created */
+            Campground.find().where("author.id").equals(foundUser._id).exec(function(err, campgrounds){
+                if(err){
+                    req.flash("error", "Error finding user");
+                    res.redirect("back");
+                } else {
+                    res.render("users/show", {user: foundUser, campgrounds: campgrounds});
+                }
+            });
+        }
+    });
+});
+
+/* EDIT - User profile */
+router.get("/user/edit/:id", function(req, res){
+    User.findById(req.params.id, function(err, foundUser){
+        if(err){
+            req.flash("error", "Error finding user");
+            res.redirect("back");
+        } else {
+            res.render("users/edit", {user: foundUser});
+        }
+    });
+});
+
+/* UPDATE - User profile */
+router.put("/user/:id", function(req, res){
+    User.findByIdAndUpdate(req.params.id, req.body.user, function(err, user){
+        if(err){
+            req.flash("error", "Error finding user");
+            res.redirect("back");
+        } else {
+            req.flash("success", "Successfully updated your profile!");
+            res.redirect("/user/" + user._id);
+        }
+    });
+});
+
 
 module.exports = router;
